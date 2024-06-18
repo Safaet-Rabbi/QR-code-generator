14805c8e7791d06ed9bbbcc8d724c95ae7be5267
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-18 22:05:44 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import FormInput from './components/Form/FormInput';
+
+const App = () => {
+  return (
+    <div>
+      <FormInput />
+    </div>
+  );
+};
+
+export default App;
